e60195fe078fa65660a9f768e7d0d8451aee2468
stanisdorg <stasdoroganov@gmail.com>
2026-01-28 00:15:26 +0300

    Fix: Add missing imports for EF logic in tabs-navigation.js and bump version to v35

--- ui-variants/tabs-navigation.js
+++ ui-variants/tabs-navigation.js
@@ -5,6 +5,7 @@ import { uniqueQaData } from '../all-data.js';
 import { buildCategoriesFromData } from '../computed-categories.js';
 import { setNormalizationDisabled } from '../load-json-data.js';
 import { getProgressMap } from '../srs/storage.js';
+import { getDifficultyLevel, getLevelProgress } from '../srs/algorithm.js';
 
 // Глобальные флаги/состояния для режима редактирования и логина
 let editMode = (typeof localStorage !== 'undefined' && localStorage.getItem('qaEditMode') === 'true') ? true : false;
