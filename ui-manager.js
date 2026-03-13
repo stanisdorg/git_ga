54bb7077f6b47b13f73b90f058edb617c40d94a8
stanisdorg <stasdoroganov@gmail.com>
2026-03-13 03:07:51 +0300

    🔖 Обновлена версия приложения до 4.24

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '4.23';
+export const APP_VERSION = '4.24';
 
 let uiInitialized = false;
 
