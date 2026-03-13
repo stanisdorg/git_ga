61ed167dcf6d60eafd7b7db1a62638adc261a741
stanisdorg <stasdoroganov@gmail.com>
2026-03-13 06:32:45 +0300

    🔖 Версия 4.31

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '4.30';
+export const APP_VERSION = '4.31';
 
 let uiInitialized = false;
 
