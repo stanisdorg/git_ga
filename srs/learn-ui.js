3c63cafeff203be807b8722a3c78028901505c4a
stanisdorg <stasdoroganov@gmail.com>
2026-03-18 04:13:03 +0300

    v4.58-beta: mobile UI improvements + Fade+Flip animation

--- srs/learn-ui.js
+++ srs/learn-ui.js
@@ -1046,7 +1046,7 @@ function showStats(stats, results, total) {
             // const learnContainer уже объявлена выше, не нужно объявлять снова
 
             // Импортируем и вызываем initStatsPage
-            import('./stats-ui.js?v=4.57-beta').then(({ initStatsPage }) => {
+            import('./stats-ui.js?v=4.58-beta').then(({ initStatsPage }) => {
                 initStatsPage(window.currentAppVersion || '4.50-beta');
             }).catch(err => {
                 console.error('[STATS BUTTON] Failed to load stats-ui:', err);
