d40859cfe87e1fccb20bcc8293a6d6e277700fec
stanisdorg <stasdoroganov@gmail.com>
2026-03-19 16:43:18 +0300

    v4.63: Skeleton loader для страницы статистики

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '4.62';
+export const APP_VERSION = '4.63';
 
 let uiInitialized = false;
 
