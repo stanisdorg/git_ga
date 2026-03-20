18fcb18d17c061cd8d3412965372747e68402e13
stanisdorg <stasdoroganov@gmail.com>
2026-03-21 02:42:08 +0300

    v5.01: Релизная версия

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '5.00-fix-logout-progress';
+export const APP_VERSION = '5.01';
 
 let uiInitialized = false;
 
