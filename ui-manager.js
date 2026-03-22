7bb8b8f4816bca0818872e948bae970885cfd307
stanisdorg <stasdoroganov@gmail.com>
2026-03-22 03:36:11 +0300

    v5.12: Обновление версии (toolbar 4+7+8, адаптив)

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '5.11';
+export const APP_VERSION = '5.12';
 
 let uiInitialized = false;
 
