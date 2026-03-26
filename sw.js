0b42f09d3045dbe674997447089d55c6fcb0f0fa
stanisdorg <stasdoroganov@gmail.com>
2026-03-27 00:07:59 +0300

    Fix inline styles with setProperty + important + bump cache to v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bytecards-v19';  // Увеличиваем версию при изменениях статики
+const CACHE_NAME = 'bytecards-v20';  // Увеличиваем версию при изменениях статики
 
 // Ресурсы для кэширования (статика)
 const STATIC_ASSETS = [
