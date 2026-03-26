f49bb216e8fbe9a7919babc7b2108d3662e14581
stanisdorg <stasdoroganov@gmail.com>
2026-03-27 00:28:11 +0300

    Add margin-top to first child to avoid header overlap + bump cache to v23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bytecards-v22';  // Увеличиваем версию при изменениях статики
+const CACHE_NAME = 'bytecards-v23';  // Увеличиваем версию при изменениях статики
 
 // Ресурсы для кэширования (статика)
 const STATIC_ASSETS = [
