4c7f51eeaf1aceb98018919ef4d0f37892ab8ad1
stanisdorg <stasdoroganov@gmail.com>
2026-03-27 01:08:38 +0300

    Fix: move top-bar down to 50px + add SW update listener + bump cache to v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bytecards-v27';  // Увеличиваем версию при изменениях статики
+const CACHE_NAME = 'bytecards-v28';  // Увеличиваем версию при изменениях статики
 
 // Ресурсы для кэширования (статика)
 const STATIC_ASSETS = [
