b222645ab6b1a0c882136135396f86b08a261d96
stanisdorg <stasdoroganov@gmail.com>
2026-03-27 02:20:26 +0300

    Bump app version to 6.13.0 + cache v35

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8085;
 const IP = '0.0.0.0'; // Слушаем на всех интерфейсах
-const VERSION = '6.10.0';
+const VERSION = '6.13.0';
 
 console.log('========================================');
 console.log(`[SERVER] Starting QA Assistant v${VERSION}...`);
