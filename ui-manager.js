53801184d24a49c366cc7e0d3c0441782bab3d85
stanisdorg <stasdoroganov@gmail.com>
2026-04-02 07:10:02 +0300

    style: gap 6px, шрифт 13px

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '6.26.6';
+export const APP_VERSION = '6.26.7';
 
 let uiInitialized = false;
 
