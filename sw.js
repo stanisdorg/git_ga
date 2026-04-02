d9b2858d83d14253b3b285f7e0ab8c4ca687b62e
stanisdorg <stasdoroganov@gmail.com>
2026-04-02 04:58:38 +0300

    style: компактная обводка поисковой подсветки

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bytecards-v48';  // Увеличиваем версию при изменениях статики
+const CACHE_NAME = 'bytecards-v49';  // Увеличиваем версию при изменениях статики
 
 // Ресурсы для кэширования (статика)
 const STATIC_ASSETS = [
