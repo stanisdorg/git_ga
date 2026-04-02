5103d0b00fba03924711a0874757547f96b9c80a
stanisdorg <stasdoroganov@gmail.com>
2026-04-02 07:01:46 +0300

    style: уменьшенные отступы панели и чипсов

--- ui-manager.js
+++ ui-manager.js
@@ -6,7 +6,7 @@ import { initStatsPage, hideStatsPage } from './srs/stats-ui.js';
 import { loadFromServer } from './srs/storage.js';
 import { initSyncIndicator } from './srs/sync-ui.js';
 
-export const APP_VERSION = '6.26.5';
+export const APP_VERSION = '6.26.6';
 
 let uiInitialized = false;
 
